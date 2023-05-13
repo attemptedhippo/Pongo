b00691fdffa40c8499eb32a9e9703039ee91d2d5
Devon <attemptedhippo@gmail.com>
2023-05-13 15:25:37 -0400

    idk what was changed here, probably just fixing block spacing

--- src/main.cpp
+++ src/main.cpp
@@ -20,19 +20,23 @@ int main()
 	std::vector<std::unique_ptr<Block>> blocks;
 	int blockWidth = 50;
 	int blockHeight = 30;
+	int width_of_block_group = ((10 * blockWidth) + 5 * 9); // (i * blockWidth) + 3 * (i - 1)
+	int leftPadding = (screenWidth / 2) - width_of_block_group / 2;
+	
 	std::string Log;
+	Log.append(TextFormat("total width %i: %i  || ", width_of_block_group, leftPadding));
+
 	for(int i = 0; i < 10; i++)
 	{
-		Log.append(TextFormat("%i ", i + 3 * i));
-		blocks.push_back(std::make_unique<Block>((i * blockWidth) + 3 * i, 50, blockWidth, blockHeight));
+		blocks.push_back(std::make_unique<Block>(leftPadding + ((i * blockWidth) + 5 * i), 50, blockWidth, blockHeight));
 	}
 
 	while (!WindowShouldClose())
 	{
 		float deltaTime = GetFrameTime();
 
-		if(IsKeyDown(KEY_RIGHT)) playerPaddle.move(200 * deltaTime);
-		if(IsKeyDown(KEY_LEFT)) playerPaddle.move(-200 * deltaTime);
+		if(IsKeyDown(KEY_RIGHT)) playerPaddle.move(250 * deltaTime);
+		if(IsKeyDown(KEY_LEFT)) playerPaddle.move(-250 * deltaTime);
 
 		switch (GetKeyPressed())
 		{
@@ -41,6 +45,14 @@ int main()
 				break;
 		}
 		
+		for(auto & element : blocks)
+		{
+			if(CheckCollisionRecs(mainBall.getRect(), element->getRect()))
+			{
+				mainBall.collided(true, true);	//TODO: Break down ball.checkCollision into check and do logic. call do collide logic here.
+				element->collided();			//TODO: Implement block break logic.
+			}
+		}
 		mainBall.checkCollision(playerPaddle);
 		mainBall.update(deltaTime);
 
